90c6868aae461b75881d441f8a1355667822f24e
Itamar Tati <itamar.tati1994@gmail.com>
2019-11-27 21:03:32 +0000

    Updates

---
+++ precache-manifest.43d220073aa705f6604ed7fd8b2e18f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3abec3aa9bfa0955cdf2330ecada241",
+    "url": "/Adludio/index.html"
+  },
+  {
+    "revision": "f80e9923599ab8dbe4f4",
+    "url": "/Adludio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df1286a475310de82e71",
+    "url": "/Adludio/static/js/2.6e81fa58.chunk.js"
+  },
+  {
+    "revision": "f80e9923599ab8dbe4f4",
+    "url": "/Adludio/static/js/main.9088d987.chunk.js"
+  },
+  {
+    "revision": "997f002f7c3980d8f12d",
+    "url": "/Adludio/static/js/runtime-main.2384b7aa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Adludio/static/media/logo.25bf045c.svg"
+  }
+]);
